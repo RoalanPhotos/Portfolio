ac9e24d17ac64c95a696b79f501e07721a22693c
Vladimir Khusainov <vladimir@khusainov.fun>
2020-04-19 13:16:23 +0800

    Update common.js
    row 7, changed -
    original:   fa = '#gadget-dock',
    new:   fa = '#pages',
    Comment: gadget is moved from "gadget dock" to "pages" container

--- common.js
+++ common.js
@@ -4,7 +4,7 @@
   ca = '#editor-frame .editor-control',
   da = '#editor-frame .editor-editor',
   ea = '#editor-frame .template[data-template="',
-  fa = '#pages',
+  fa = '#pages', // gadget is moved from "gadget dock" to "pages" container
   ha = '#lightbox',
   ja = '#nyanyanya',
   ka = '#overview',
